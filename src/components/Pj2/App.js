e75bd6da3723ab578de916eaaf4b728c57688813
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-08-04 23:44:44 +0900

    t

--- src/components/Pj2/App.js
+++ src/components/Pj2/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React , { useState , useEffect } from 'react'
 
 const App = () => {
+
+    const [count, setCount] = useState(0);
+    const [age, setAge] = useState(42);
+    const [fruit, setFruit] = useState('banana');
+    const [todos, setTodos] = useState([{text : 'learn hooks'}]);
+
+    // componentDidMount, componentDidUpdate와 비슷
+    useEffect(()=> {
+        document.title = `You clicked ${count} times!`;
+    })
+
     return (
         <div>
-            <h1>pj2</h1>
+            <p>you clicked { count } times</p>
+            <button onClick={() => setCount(count + 1)}>
+                click me!
+            </button>
         </div>
     )
 }
